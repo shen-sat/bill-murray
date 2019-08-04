a562c23f5892527118d364159c1bc31fb8d636f0
Shen Sat <shensat18@gmail.com>
2019-08-04 21:57:26 +0100

    Add validations for name and email in User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,24 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "should have name present" do
+  	@user.name = " "
+  	assert_not @user.valid?
+  end
+
+  test "should have email present" do
+  	@user.email = " "
+  	assert_not @user.valid?
+  end
+
+
 end
