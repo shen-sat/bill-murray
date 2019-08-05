ba962bcd3d1e2558f46f92162006b6a47c1e8a36
Shen Sat <shensat18@gmail.com>
2019-08-05 12:33:02 +0100

    Add validation for name length, email length and email format

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,5 +23,35 @@ class UserTest < ActiveSupport::TestCase
   	assert_not @user.valid?
   end
 
+  test "email should not be too long" do
+    @user.email = "a" * 255 + "@example.com"
+    assert_not @user.valid? 
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid? 
+  end
+
+  test "should accept valid email addresses" do
+    valid_emails = %w[example@example.com EXAMPLE@example.com exa_mple@example.com example@foo.jp]
+    valid_emails.each do |email|
+      @user.email = email
+      assert @user.valid?, "This email: #{email.inspect} should be valid"
+    end 
+  end
+
+  test "should accept invalid email addresses" do
+    valid_emails = %w[example.com EXAMPLE_at_example.com exa_mple@example. example@foo+jp]
+    valid_emails.each do |email|
+      @user.email = email
+      assert_not @user.valid?, "This email: #{email.inspect} should be invalid"
+    end 
+  end
+
+
+
+
+
 
 end
