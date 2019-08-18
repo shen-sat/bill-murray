f9ecdf528eadf70159f181dae4225bdae3c0e8f6
Shen Sat <shensat18@gmail.com>
2019-08-18 10:37:02 +0100

    Add a sessions resource and a form for login. Onwards to 8.1.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get '/signup', 	to: 'users#new'
 
   get '/home', 		to: 'static_pages#home' 
@@ -7,10 +8,14 @@ Rails.application.routes.draw do
   
   get '/about', 	to: 'static_pages#about'
   
-  get '/contact', 	to: 'static_pages#contact'
-  post '/signup',  	to: 'users#create'
+  get '/contact', to: 'static_pages#contact'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
   resources :users
+
+  get '/login',      to: 'sessions#new' 
+  post '/login',     to: 'sessions#create' 
+  delete '/logout',  to: 'sessions#destroy' 
 end
