428d63695260246439558ba8facaaa0e30fe8403
Shen Sat <shensat18@gmail.com>
2019-08-20 11:31:52 +0100

    Add valid logging handling and logged-in layout and changed tests to use root_path instead of home_path

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,32 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "danger flash persists for only one request" do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path, params: { session: { email: "invalid", password: "short" } }
-    assert_template 'sessions/new'
-    assert flash[:danger]
-    get home_path
-    assert flash.empty?
-  end
+	
+	def setup
+		@user = users(:michael)
+	end
+
+
+	test "danger flash persists for only one request" do
+		get login_path
+		assert_template 'sessions/new'
+		post login_path, params: { session: { email: "invalid", password: "short" } }
+		assert_template 'sessions/new'
+		assert flash[:danger]
+		get home_path
+		assert flash.empty?
+	end
+
+	test 'should login user' do
+		puts ">>>>"
+		get login_path
+		assert_template 'sessions/new'
+		post login_path, params: { session: { email: @user.email, password: 'password' } }
+		follow_redirect!
+		assert_select "a[href=?]", login_path, count: 0
+		assert_select "a[href=?]", logout_path
+		assert_select "a[href=?]", user_path
+		
+
+	end
 end
